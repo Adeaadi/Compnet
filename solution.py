caa4620fded4cc7513e7eb2f2a63b035b64ae27b
Adeaadi <113269585+Adeaadi@users.noreply.github.com>
2022-09-10 22:49:20 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -17,9 +17,9 @@ def welcome_assignment_answers(question):
         answer = "8496abe9fceb5aa927e28bfbd9a2347d1290ef9b"
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "Yes"
-    elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+    elif question == "What layer of the TCP/IP model does the protocol DNS belong to?":
         answer = "int(input(1))"
-    elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+    elif question == "What layer of the TCP/IP model does the protocol ICMP belong to?":
         answer = "int(input(3))" 
  
     else: 
