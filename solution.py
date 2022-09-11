a381f07529c9c41cb2a848a399d149aecccf4e84
Adeaadi <113269585+Adeaadi@users.noreply.github.com>
2022-09-10 22:48:57 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -11,23 +11,21 @@ def welcome_assignment_answers(question):
         answer = "No"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
         answer = "Yes"
-     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         answer = "No"   
     elif question == "What is the SHA1 hashing value to the following message: 'NYU Computer Networking' - Use SHA1 hash generator and use the answer in your code":
         answer = "8496abe9fceb5aa927e28bfbd9a2347d1290ef9b"
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "Yes"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = "1"
+        answer = "int(input(1))"
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-        answer = "3" 
+        answer = "int(input(3))" 
  
     else: 
-        ### you should understand why this else case should be included
        
         answer = "incorrect"
     return(answer)
-# Complete all the questions.
 
 
 if __name__ == "__Adeyinka Adiatu__":
