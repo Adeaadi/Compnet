5fac8464317005ac37688bf17d92faefae52f1b3
Adeaadi <113269585+Adeaadi@users.noreply.github.com>
2022-09-24 19:16:58 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,4 +1,3 @@
-
 # import socket module
 from socket import *
 # In order to terminate the program
@@ -10,30 +9,28 @@ def webServer(port=13331):
   format='utf-8'
   disconnect_message ="!disconnect"
   #Prepare a server socket
-  serverSocket.bind(("", port))
-  
+
+
   #Fill in start
-
-  def start():
-      server.listen()
-      while true:
-          conn, addr = server.accept()
-          thread = threading.thread(target=handle_client, args=(conn, addr))
-          thread.start()
-          print(f"[active connections] {threading.activecount()-1}")
-
-  print("[starting] server is starting...")
-  start()
-
-
-#Fill in end
-
-  while True:
-    #Establish the connection
-    socket.connect(192.168.1.166, port)
+  serverSocket.bind(("", port))
+  serverSocket.listen(1)
+  print 'the web server is active and ready to receive on port:', port
+  #Fill in end
+    while True:
+      #Establish the connection
     print('Ready to serve...')
-    connectionSocket, addr = #Fill in start -are you accepting connections?     #Fill in end
-    
+    connectionSocket, addr = serversocket.accept()#Fill in start -are you accepting connections?
+    sentence = connectionSocket.recv().decode()
+    capitalizedsentence = sentence.upper()
+    connectionsocket.send(capitalizedsentence.encode())
+    connectionsocket.close()
+  #Fill in end
+
+    #socket.connect(192.168.1.166, port)
+
+
+  #print("[starting] server is starting...")
+  #start()
     try:
       message = #Fill in start -a client is sending you a message
 
@@ -54,9 +51,7 @@ def webServer(port=13331):
       
       #opens the client requested file. 
       #Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
-      f = open(filename[1:], #fill in start
-
-
+      f = open(filename[1:], "helloworld.html","r", "utf-8")#fill in start
                # #fill in end)
       #fill in end
       
