2eb40fd57076d67e55340acc360fd19343677e6d
Adeaadi <113269585+Adeaadi@users.noreply.github.com>
2022-09-24 21:39:25 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,91 +1,75 @@
 # import socket module
 from socket import *
 # In order to terminate the program
+import sys
 
 def webServer(port=13331):
   serverSocket = socket(AF_INET, SOCK_STREAM)
-  addr = (server,port)
-  header = 64
-  format='utf-8'
-  disconnect_message ="!disconnect"
-  #Prepare a server socket
 
+  #Prepare a server socket
+  serverSocket.bind(("", port))
 
   #Fill in start
-  serverSocket.bind(("", port))
-  serverSocket.listen(1)
-  print 'the web server is active and ready to receive on port:', port
-  #Fill in end
-    while True:
-      #Establish the connection
+serverSocket.listen(1)
+ #Fill in end
+
+while True:
+    #Establish the connection
     print('Ready to serve...')
-    connectionSocket, addr = serversocket.accept()#Fill in start -are you accepting connections?
+    connectionSocket, addr = serversocket.accept() #Fill in start -are you accepting connections?  #Fill in start -are you accepting connections?
     sentence = connectionSocket.recv().decode()
     capitalizedsentence = sentence.upper()
     connectionsocket.send(capitalizedsentence.encode())
-    connectionsocket.close()
-  #Fill in end
+    connectionsocket.close()    #Fill in end
 
-    #socket.connect(192.168.1.166, port)
-
-
-  #print("[starting] server is starting...")
-  #start()
     try:
-      message = #Fill in start -a client is sending you a message
+      message = connectionSocket.recv(128)
+      #Fill in start -a client is sending you a message
 
-        def handle_client(conn, addr):
-    pass
-    print(f"[new connection] {addr} connected.")
-    connected=true
-      while connected:
-        msg_length = conn.recv(header).decode(format)
-        msg_length = int(msg_length)
-        msg = conn.recv(msg_length).decode(format)
-        if msg == disconnect_message:
-            connected=false
-        print (f"[{addr}] {msg}")
-
-      # #Fill in end
+      #  #Fill in end
       filename = message.split()[1]
-      
-      #opens the client requested file. 
+
+      #opens the client requested file.
       #Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
-      f = open(filename[1:], "helloworld.html","r", "utf-8")#fill in start
-               # #fill in end)
+      f = open(filename[1:],"helloworld.html","r", "utf-8") #fill in start #fill in end)
       #fill in end
-      
+
       outputdata = b"Content-Type: text/html; charset=UTF-8\r\n"\
-      #Fill in start -This variable can store your headers you want to send for any valid or invalid request. 
+      #Fill in start -This variable can store your headers you want to send for any valid or invalid request.
+
       #Content-Type above is an example on how to send a header as bytes
       #Fill in end
 
-      #Send an HTTP header line into socket for a valid request. What header should be sent for a response that is ok? 
+      #Send an HTTP header line into socket for a valid request. What header should be sent for a response that is ok?
       #Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
       #Fill in start
+    connectionSocket.send=('\HTTP/1.1 200 OK\n\n')
+    connectionSocket.send(outputdata)
+    connectionSocket.close()
+    #Fill in end
 
-      #Fill in end
-               
 
       #Send the content of the requested file to the client
       for i in f: #for line in file
-        #Fill in start - send your html file contents #Fill in end 
+        #Fill in start - send your html file contents #Fill in end
       connectionSocket.close() #closing the connection socket
-      
-    except Exception as e:
-      # Send response message for invalid request due to the file not being found (404)
-      #Fill in start
 
+    except Exception as e:
+        # Send response message for invalid request due to the file not being found (404)
+      #Fill in start
+        pass
+        print "404 Not Found"
+        connectionsocket.send('\http/1.1 404 Not Found\n\n')
       #Fill in end
 
 
       #Close client socket
       #Fill in start
-     client.close()
+        client.close()
       #Fill in end
 
   serverSocket.close()
-  sys.exit()  # Terminate the program after sending the corresponding data
+sys.exit()  # Terminate the program after sending the corresponding data
 
 if __name__ == "__main__":
   webServer(13331)
