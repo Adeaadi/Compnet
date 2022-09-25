6422f72b5a03624b313bd1a8dacd3d7c3f63db7b
Adeaadi <113269585+Adeaadi@users.noreply.github.com>
2022-09-24 23:00:12 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,32 +5,30 @@ import sys
 
 def webServer(port=13331):
   serverSocket = socket(AF_INET, SOCK_STREAM)
-
   #Prepare a server socket
   serverSocket.bind(("", port))
-
   #Fill in start
-serverSocket.listen(1)
- #Fill in end
+  serverSocket.listen(1) #1 connectoin allowed
+  #Fill in end
 
-while True:
-    #Establish the connection
   while True:
     #Establish the connection
     #print('Ready to serve...')
-    connectionSocket, addr = serverSocket.accept() #Fill in start      #Fill in end
+    connectionSocket, addr = serverSocket.accept() #Fill in
+    #print(addr)
     try:
 
       try:
-        message = connectionSocket.recv(1024) #Fill in start    #Fill in end
+        message = connectionSocket.recv(1024) #recieve 1024 bits #Fill in start    #Fill in end
         filename = message.split()[1]
         f = open(filename[1:])
-        outputdata = f.read() #Fill in start     #Fill in end
-
+        outputdata = "HTTP/1.1 200 OK\r\n"#Fill in start     #Fill in end
+        connectionSocket.send(outputdata.encode())
         #Send one HTTP header line into socket.
         #Fill in start
-        header = "HTTP/1.1 200 OK"
-        connectionSocket.send(header.encode())
+        outputdata = "Content-Type: text/plain\n"
+        outputdata += ("File Data: " + f.read() + "\n")
+        #print(outputdata)
         #Fill in end
 
         #Send the content of the requested file to the client
@@ -40,16 +38,17 @@ while True:
         connectionSocket.send("\r\n".encode())
         connectionSocket.close()
       except IOError:
+
         # Send response message for file not found (404)
         #Fill in start
-        header = "HTTP/1.1 404 Not Found"
-        connectionSocket.send(header.encode())
+        message = "HTTP/1.1 404 NOT FOUND\r\n"
+        connectionSocket.send(message.encode())
         #Fill in end
 
-      #Close client socket
-      #Fill in start
-        client.close()
-      #Fill in end
+        #Close client socket
+        #Fill in start
+        connectionSocket.close()
+        #Fill in end
 
    #Commenting out the below, as its technically not required and some students have moved it erroneously in the While loop. DO NOT DO THAT OR YOURE GONNA HAVE A BAD TIME.
   #serverSocket.close()
