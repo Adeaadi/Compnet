74e0743e576922e11268c7e20e344bb8aa4531bb
Adeaadi <113269585+Adeaadi@users.noreply.github.com>
2022-09-24 23:12:48 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,54 +5,69 @@ import sys
 
 def webServer(port=13331):
   serverSocket = socket(AF_INET, SOCK_STREAM)
+  
   #Prepare a server socket
   serverSocket.bind(("", port))
+  
   #Fill in start
-  serverSocket.listen(1) #1 connectoin allowed
+  serverSocket.listen(1)
   #Fill in end
 
   while True:
     #Establish the connection
-    #print('Ready to serve...')
-    connectionSocket, addr = serverSocket.accept() #Fill in
-    #print(addr)
+    
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept() #Fill in start -are you accepting connections?     #Fill in end
+    
     try:
+      message = connectionSocket.recv(1024)#Fill in start -a client is sending you a message   #Fill in end 
+      filename = message.split()[1]
+      
+      #opens the client requested file. 
+      #Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
+      f = open(filename[1:], #fill in start #fill in end)
+      #fill in end
+      outputdata = f.read(helloworld.html)
+      #outputdata = b"Content-Type: text/html; charset=UTF-8\r\n"\
+      #Fill in start -This variable can store your headers you want to send for any valid or invalid request. 
+      #Content-Type above is an example on how to send a header as bytes
+      
+      #Fill in end
 
-      try:
-        message = connectionSocket.recv(1024) #recieve 1024 bits #Fill in start    #Fill in end
-        filename = message.split()[1]
-        f = open(filename[1:])
-        outputdata = "HTTP/1.1 200 OK\r\n"#Fill in start     #Fill in end
-        connectionSocket.send(outputdata.encode())
-        #Send one HTTP header line into socket.
-        #Fill in start
-        outputdata = "Content-Type: text/plain\n"
-        outputdata += ("File Data: " + f.read() + "\n")
-        #print(outputdata)
-        #Fill in end
+      #Send an HTTP header line into socket for a valid request. What header should be sent for a response that is ok? 
+      #Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
+      #Fill in start
+      connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
+      #Fill in end
+               
 
-        #Send the content of the requested file to the client
-        for i in range(0, len(outputdata)):
-          connectionSocket.send(outputdata[i].encode())
+      #Send the content of the requested file to the client
+      for i in f: #for line in file
+        #Fill in start - send your html file contents #Fill in end 
+      connectionSocket.close() #closing the connection socket
+      
+    except Exception as e:
+      # Send response message for invalid request due to the file not being found (404)
+      #Fill in start
+      connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
+      #Fill in end
 
-        connectionSocket.send("\r\n".encode())
-        connectionSocket.close()
-      except IOError:
 
-        # Send response message for file not found (404)
-        #Fill in start
-        message = "HTTP/1.1 404 NOT FOUND\r\n"
-        connectionSocket.send(message.encode())
-        #Fill in end
+      #Close client socket
+      #Fill in start
+      connectionsocket.close()
+      #Fill in end
 
-        #Close client socket
-        #Fill in start
-        connectionSocket.close()
-        #Fill in end
-
-   #Commenting out the below, as its technically not required and some students have moved it erroneously in the While loop. DO NOT DO THAT OR YOURE GONNA HAVE A BAD TIME.
+  #Commenting out the below, as its technically not required and some students have moved it erroneously in the While loop. DO NOT DO THAT OR YOURE GONNA HAVE A BAD TIME.
   #serverSocket.close()
   #sys.exit()  # Terminate the program after sending the corresponding data
 
 if __name__ == "__main__":
   webServer(13331)
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
